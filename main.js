3cc4cbbac991449fbd664b3d7fbc9afd9bc105a4
nellygreen <nellygreen.cg@gmail.com>
2025-01-13 19:00:38 +0100

    update

--- main.js
+++ main.js
@@ -71,3 +71,28 @@ const swiper = new Swiper('.swiper', {
     }
   });
   
+  /* ACTIVE LINK */
+  const activeLink = () => {
+    const sections = document.querySelectorAll('section')
+    const navLinks = document.querySelectorAll(".nav-link")
+
+    let current = "home"
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+
+        if(this.scrollY >= sectionTop - 60) {
+            current = section.getAttribute("id")
+        }
+    })
+
+    navLinks.forEach(item => {
+        item.classList.remove("active")
+        if(item.href.includes(current)) {
+            item.classList.add("active")
+        }
+    })
+    
+  }
+
+  window.addEventListener("scroll", activeLink)
